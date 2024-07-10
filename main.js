f8fe6f481265334875d8e6c1e43dc6e7cc0e6c6c
younghwa <xxnewnewxx0127@gmail.com>
2024-07-10 10:46:22 +0900

    two

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-const API_KEY = `29a96ef9b757480189bbd02b34691b57`;
+// const API_KEY = `29a96ef9b757480189bbd02b34691b57`;
 
+// let news = [];
+// const getLatesNews = async () => {
+//   const url = new URL(
+//     `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}`
+//   );
+//   //   console.log("uuu", url);
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   news = data.articles;
+//   console.log("ddddddd", news);
+// };
+
+// getLatesNews();
+
+const api_key = "140c1074224f4a21ad30e882f4092dfc";
 let news = [];
-const getLatesNews = async () => {
+const pageSize = 10;
+let page = 2;
+let category = "";
+let keyword = "";
+const getLatestNews = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}`
+    `https://stephnews.netlify.app/top-headlines?country=us&pageSize=${pageSize}&page=${page}${category}${keyword}`
   );
-  //   console.log("uuu", url);
+
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
-  console.log("ddddddd", news);
+  console.log("news=", news);
 };
 
-getLatesNews();
+getLatestNews();
